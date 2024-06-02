e1e8fa3a750227a999b1ab05d3079979a4bf914b
Filbog <f.boguslawski101@gmail.com>
2024-06-02 15:54:51 +0200

    all the awesome functionality added, now onto tracker, displaying stats, and styling/visual aspects

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from .views import (
     ActivityStatisticsView,
     ActivityUpdateView,
     ActivityDeleteView,
-    ActivityCreateView,
+    # ActivityCreateView,
 )
 
 urlpatterns = [
@@ -28,5 +28,5 @@ urlpatterns = [
         ActivityDeleteView.as_view(),
         name="activity_delete",
     ),
-    path("activities/create/", ActivityCreateView.as_view(), name="activity_create"),
+    # path("activities/create/", ActivityCreateView.as_view(), name="activity_create"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,26 +1,73 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Activity
+from .forms import ActivityForm
+
+
+class AuthMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.username == self.get_object().owner.username
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
 
-class ActivityListView(ListView):
+class ActivityGet(ListView):
     model = Activity
     template_name = "activity_list.html"
 
+    def get_queryset(self):
+        return Activity.objects.filter(owner=self.request.user)
 
-class ActivityStatisticsView(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = ActivityForm()
+        return context
+
+
+class ActivityPost(FormView):
+    # model = Activity
+    form_class = ActivityForm
+    template_name = "activity_list.html"
+
+    def post(self, request, *args, **kwargs):
+        # self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        activity = form.save(commit=False)
+        activity.owner = self.request.user
+        activity.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        # activity = self.object
+        return reverse("activity_list")
+
+
+class ActivityListView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = ActivityGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = ActivityPost.as_view()
+        return view(request, *args, **kwargs)
+
+
+class ActivityStatisticsView(LoginRequiredMixin, AuthMixin, DetailView):
     model = Activity
     template_name = "activity_statistics.html"
 
 
-class ActivityUpdateView(UpdateView):
+class ActivityUpdateView(LoginRequiredMixin, AuthMixin, UpdateView):
     model = Activity
     fields = (
         "title",
@@ -30,17 +77,18 @@ class ActivityUpdateView(UpdateView):
     template_name = "activity_edit.html"
 
 
-class ActivityDeleteView(DeleteView):
+class ActivityDeleteView(LoginRequiredMixin, AuthMixin, DeleteView):
     model = Activity
     template_name = "activity_delete.html"
     success_url = reverse_lazy("activity_list")
 
 
-class ActivityCreateView(CreateView):
-    model = Activity
-    template_name = "activity_create.html"
-    fields = ("title", "description", "type")
+# class ActivityCreateView(LoginRequiredMixin, CreateView):
+#     model = Activity
+#     form_class = ActivityForm
+#     template_name = "activity_list.html"
+#     success_url = reverse_lazy("activity_list")
 
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         form.instance.owner = self.request.user
+#         return super().form_valid(form)
