29293cdf6912df91c2bbeeb1700c807b532c5fcb
Filbog <f.boguslawski101@gmail.com>
2024-06-14 02:49:27 +0200

    last touches before deploy

--- ta_project/settings.py
+++ ta_project/settings.py
@@ -158,4 +158,12 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 # email stuff
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+if DEBUG == True:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
